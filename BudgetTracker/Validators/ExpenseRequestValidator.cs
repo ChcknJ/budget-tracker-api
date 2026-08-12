d674f81fab7b3f7542eaec2db7e5c1dbb81fd775
Kyle <kyleesurena1@gmail.com>
2026-08-12 22:19:51 +0800

    add validators: expense, category, subscription, budget

--- BudgetTracker/Validators/ExpenseRequestValidator.cs
+++ BudgetTracker/Validators/ExpenseRequestValidator.cs
@@ -1,6 +1,19 @@
-﻿namespace BudgetTracker.Validators
+﻿using BudgetTracker.DTO;
+using FluentValidation;
+
+
+namespace BudgetTracker.Validators
 {
-    public class ExpenseRequestValidator :
+    public class ExpenseRequestValidator : AbstractValidator<ExpenseRequest>
     {
+        public ExpenseRequestValidator()
+        {
+            RuleFor(x => x.CategoryId).NotEmpty().WithMessage("Category is required.");
+            RuleFor(x => x.Amount).GreaterThan(0).WithMessage("Amount must be greater than 0.");
+            RuleFor(x => x.Description).MaximumLength(200).WithMessage("Description cannot exceed 200 characters.");
+            RuleFor(x => x.Date).NotEmpty().WithMessage("Date is required");
+
+            
+        }
     }
 }
---
+++ BudgetTracker/Validators/SubscriptionRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using BudgetTracker.DTO;
+using FluentValidation;
+
+namespace BudgetTracker.Validators
+{
+    public class SubscriptionRequestValidator : AbstractValidator<SubscriptionRequest>
+    {
+        public SubscriptionRequestValidator()
+        {
+            RuleFor(x => x.CategoryId).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required").MaximumLength(100).WithMessage("Name cannot exceed 100 characters.");
+            RuleFor(x => x.Amount).GreaterThan(0).WithMessage("Amount must be greater than 0.");
+            RuleFor(x => x.StartDate).NotEmpty().WithMessage("Start date is required.");
+            RuleFor(x => x.EndDate).GreaterThanOrEqualTo(x => x.StartDate).When(x => x.EndDate.HasValue).WithMessage("End date must be after Start Date");
+            RuleFor(x => x.BillingCycle).NotEmpty().WithMessage("Billing Cycle is required.").Must(x => x == "Weekly" || x == "Monthly" || x == "Yearly").WithMessage("Billing cycle must be Weekly, Monthly, or Yearly.");
+        }
+    }
+}
