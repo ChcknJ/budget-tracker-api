0008ddfd440e6de10a9392dc081ee62ee6834ad6
Kyle <kyleesurena1@gmail.com>
2026-08-15 14:51:02 +0800

    complete summary feature

---
+++ BudgetTracker/Controllers/SummaryController.cs
@@ -0,0 +1,53 @@
+﻿using BudgetTracker.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BudgetTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SummaryController : ControllerBase
+    {
+        private readonly ISummaryService _summaryService;
+
+        public SummaryController(ISummaryService summary)
+        {
+            _summaryService = summary;
+        }
+
+
+        [Authorize]
+        [HttpGet("get-summary")]
+        public async Task<IActionResult> GetSummaryAsync(DateOnly month)
+        {
+            var userId = GetUserId();
+
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+
+            var response = await _summaryService.GetSummaryAsync(userId.Value, month);
+            return Ok(response);
+        }
+
+        private Guid? GetUserId()
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (userIdClaim == null)
+            {
+                return null;
+            }
+
+            if (!Guid.TryParse(userIdClaim.Value, out Guid userId))
+            {
+                return null;
+            }
+
+            return userId;
+        }
+    }
+}
