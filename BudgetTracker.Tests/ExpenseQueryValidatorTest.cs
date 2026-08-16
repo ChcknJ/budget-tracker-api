c647cb7dffff523794e6652f855039e1d70fd5d1
Kyle <kyleesurena1@gmail.com>
2026-08-16 19:53:07 +0800

    complete unit tests for validators

---
+++ BudgetTracker.Tests/ExpenseQueryValidatorTest.cs
@@ -0,0 +1,154 @@
+﻿using BudgetTracker.DTO;
+using BudgetTracker.Validators;
+using FluentValidation.TestHelper;
+
+namespace BudgetTracker.Tests
+{
+    public class ExpenseQueryValidatorTest
+    {
+        [Fact]
+        public void ExpenseQueryValidator_ValidRequest_ShouldPassValidation()
+        {
+            var query = new ExpenseQueryRequest
+            {
+                CategoryId = Guid.NewGuid(),
+                SubscriptionId = Guid.NewGuid(),
+                FromDate = DateOnly.FromDateTime(DateTime.Today),
+                ToDate = DateOnly.FromDateTime(DateTime.Today),
+                MinimumAmount = 100,
+                MaximumAmount = 200,
+                PageNumber = 1,
+                PageSize = 20,
+                SortBy = "date",
+                SortOrder = "ascending",
+            };
+
+            var validator = new ExpenseQueryRequestValidator();
+
+            var result = validator.TestValidate(query);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void ExpenseQueryValidator_MinimumAmountGreaterThanMaximumAmount_ShouldHaveValidationError()
+        {
+            var query = new ExpenseQueryRequest
+            {
+                CategoryId = Guid.NewGuid(),
+                SubscriptionId = Guid.NewGuid(),
+                FromDate = DateOnly.FromDateTime(DateTime.Today),
+                ToDate = DateOnly.FromDateTime(DateTime.Today),
+                MinimumAmount = 300,
+                MaximumAmount = 200,
+                PageNumber = 1,
+                PageSize = 20,
+                SortBy = "date",
+                SortOrder = "ascending",
+            };
+
+            var validator = new ExpenseQueryRequestValidator();
+
+            var result = validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x=>x.MinimumAmount);
+        }
+
+        [Fact]
+        public void ExpenseQueryValidator_InvalidSortBy_ShouldHaveValidationError()
+        {
+            var query = new ExpenseQueryRequest
+            {
+                CategoryId = Guid.NewGuid(),
+                SubscriptionId = Guid.NewGuid(),
+                FromDate = DateOnly.FromDateTime(DateTime.Today),
+                ToDate = DateOnly.FromDateTime(DateTime.Today),
+                MinimumAmount = 100,
+                MaximumAmount = 200,
+                PageNumber = 1,
+                PageSize = 20,
+                SortBy = "Date",
+                SortOrder = "ascending",
+            };
+
+            var validator = new ExpenseQueryRequestValidator();
+
+            var result = validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.SortBy);
+        }
+
+        [Fact]
+        public void ExpenseQueryValidator_InvalidSortOrder_ShouldHaveValidationError()
+        {
+            var query = new ExpenseQueryRequest
+            {
+                CategoryId = Guid.NewGuid(),
+                SubscriptionId = Guid.NewGuid(),
+                FromDate = DateOnly.FromDateTime(DateTime.Today),
+                ToDate = DateOnly.FromDateTime(DateTime.Today),
+                MinimumAmount = 100,
+                MaximumAmount = 200,
+                PageNumber = 1,
+                PageSize = 20,
+                SortBy = "date",
+                SortOrder = "Ascending",
+            };
+
+            var validator = new ExpenseQueryRequestValidator();
+
+            var result = validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.SortOrder);
+        }
+
+        [Fact]
+        public void ExpenseQueryValidator_PageSizeOver20_ShouldHaveValidationError()
+        {
+            var query = new ExpenseQueryRequest
+            {
+                CategoryId = Guid.NewGuid(),
+                SubscriptionId = Guid.NewGuid(),
+                FromDate = DateOnly.FromDateTime(DateTime.Today),
+                ToDate = DateOnly.FromDateTime(DateTime.Today),
+                MinimumAmount = 100,
+                MaximumAmount = 200,
+                PageNumber = 1,
+                PageSize = 30,
+                SortBy = "date",
+                SortOrder = "ascending",
+            };
+
+            var validator = new ExpenseQueryRequestValidator();
+
+            var result = validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.PageSize);
+        }
+
+
+        [Fact]
+        public void ExpenseQueryValidator_PageNumberLessThan1_ShouldHaveValidationError()
+        {
+            var query = new ExpenseQueryRequest
+            {
+                CategoryId = Guid.NewGuid(),
+                SubscriptionId = Guid.NewGuid(),
+                FromDate = DateOnly.FromDateTime(DateTime.Today),
+                ToDate = DateOnly.FromDateTime(DateTime.Today),
+                MinimumAmount = 100,
+                MaximumAmount = 200,
+                PageNumber = 0,
+                PageSize = 20,
+                SortBy = "date",
+                SortOrder = "ascending",
+            };
+
+            var validator = new ExpenseQueryRequestValidator();
+
+            var result = validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.PageNumber);
+        }
+    }
+}
--- BudgetTracker/Validators/ExpenseRequestValidator.cs
+++ BudgetTracker/Validators/ExpenseRequestValidator.cs
@@ -10,7 +10,7 @@ namespace BudgetTracker.Validators
         {
             RuleFor(x => x.CategoryId).NotEmpty().WithMessage("Category is required.");
             RuleFor(x => x.Amount).GreaterThan(0).WithMessage("Amount must be greater than 0.");
-            RuleFor(x => x.Description).MaximumLength(200).WithMessage("Description cannot exceed 200 characters.");
+            RuleFor(x => x.Description).MaximumLength(100).WithMessage("Description cannot exceed 100 characters.");
             RuleFor(x => x.Date).NotEmpty().WithMessage("Date is required");
 
             
